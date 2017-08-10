56ba39a89e73b49148ed4b4e53b352aaeeeaa5ec
huyenhoang <huyenhoang33@gmail.com>
2017-08-10 16:07:55 -0700

    Update entertainment.py
    Added #NOQA to long urls.

--- Project 1 - Movie Trailer Website/entertainment.py
+++ Project 1 - Movie Trailer Website/entertainment.py
@@ -5,37 +5,37 @@ import media
 
 great_gatsby = media.Movie(
   "The Great Gatsby (1974)", 
-  "https://upload.wikimedia.org/wikipedia/en/f/fc/Great_gatsby_74.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/f/fc/Great_gatsby_74.jpg",  # NOQA
   "https://www.youtube.com/watch?v=L-SGqutpxc0"
   )
 
 ferris_bueller = media.Movie(
   "Ferris Bueller's Day Off", 
-  "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg", # NOQA
   "https://www.youtube.com/watch?v=D6gABQFR94U"
   )
 
 holes = media.Movie(
   "Holes", 
-  "https://upload.wikimedia.org/wikipedia/en/d/d6/Holesposter03.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/d/d6/Holesposter03.jpg", # NOQA
   "https://www.youtube.com/watch?v=NEvLRtDKT0c"
   )
 
 good_will_hunting = media.Movie(
   "Good Will Hunting", 
-  "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg", # NOQA
   "https://www.youtube.com/watch?v=PaZVjZEFkRs"
   )
 
 king_speech = media.Movie(
   "The King's Speech", 
-  "https://upload.wikimedia.org/wikipedia/en/a/a0/Kings_speech_ver3.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/a/a0/Kings_speech_ver3.jpg", # NOQA
   "https://www.youtube.com/watch?v=R3okvbIlbQY"
   )
 
 passengers = media.Movie(
   "Passenagers", 
-  "https://upload.wikimedia.org/wikipedia/en/8/8e/Passengers_2016_film_poster.jpg", 
+  "https://upload.wikimedia.org/wikipedia/en/8/8e/Passengers_2016_film_poster.jpg", # NOQA
   "https://www.youtube.com/watch?v=7BWWWQzTpNU"
   )
 
