98a2943b3a063f672605e4ff34703e3dff5dfae2
huyenhoang <huyenhoang33@gmail.com>
2017-08-10 16:15:30 -0700

    Update entertainment.py
    PEP8 passed.

--- Project 1 - Movie Trailer Website/entertainment.py
+++ Project 1 - Movie Trailer Website/entertainment.py
@@ -1,47 +1,55 @@
 import fresh_tomatoes
 import media
 
-# Create new instances of Movie for six movies, taking in their Name, Poster Image, and YouTuber Trailer #
+# Create new instances of Movie for six movies,
+# taking in their Name, Poster Image, and YouTuber Trailer
 
 great_gatsby = media.Movie(
-  "The Great Gatsby (1974)", 
+  "The Great Gatsby (1974)",
   "https://upload.wikimedia.org/wikipedia/en/f/fc/Great_gatsby_74.jpg",  # NOQA
   "https://www.youtube.com/watch?v=L-SGqutpxc0"
   )
 
 ferris_bueller = media.Movie(
-  "Ferris Bueller's Day Off", 
-  "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg", # NOQA
+  "Ferris Bueller's Day Off",
+  "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg",  # NOQA
   "https://www.youtube.com/watch?v=D6gABQFR94U"
   )
 
 holes = media.Movie(
-  "Holes", 
-  "https://upload.wikimedia.org/wikipedia/en/d/d6/Holesposter03.jpg", # NOQA
+  "Holes",
+  "https://upload.wikimedia.org/wikipedia/en/d/d6/Holesposter03.jpg",  # NOQA
   "https://www.youtube.com/watch?v=NEvLRtDKT0c"
   )
 
 good_will_hunting = media.Movie(
-  "Good Will Hunting", 
-  "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg", # NOQA
+  "Good Will Hunting",
+  "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg",  # NOQA
   "https://www.youtube.com/watch?v=PaZVjZEFkRs"
   )
 
 king_speech = media.Movie(
-  "The King's Speech", 
-  "https://upload.wikimedia.org/wikipedia/en/a/a0/Kings_speech_ver3.jpg", # NOQA
+  "The King's Speech",
+  "https://upload.wikimedia.org/wikipedia/en/a/a0/Kings_speech_ver3.jpg",  # NOQA
   "https://www.youtube.com/watch?v=R3okvbIlbQY"
   )
 
 passengers = media.Movie(
-  "Passenagers", 
-  "https://upload.wikimedia.org/wikipedia/en/8/8e/Passengers_2016_film_poster.jpg", # NOQA
+  "Passenagers",
+  "https://upload.wikimedia.org/wikipedia/en/8/8e/Passengers_2016_film_poster.jpg",  # NOQA
   "https://www.youtube.com/watch?v=7BWWWQzTpNU"
   )
 
-# The movies array contains a list of the movies to be displayed by the HTML code within fresh_tomatoes
-movies = [great_gatsby, ferris_bueller, holes, good_will_hunting, king_speech, passengers]
+# Movies array contains list of movies
+# to be displayed by HTML codes within fresh_tomatoes
+
+movies = [
+  great_gatsby,
+  ferris_bueller,
+  holes,
+  good_will_hunting,
+  king_speech,
+  passengers
+]
 
 fresh_tomatoes.open_movies_page(movies)
-
-
