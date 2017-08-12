4243f6e8cff69d4e397258dff9f3677a4cb68927
huyenhoang <huyenhoang33@gmail.com>
2017-08-11 21:52:23 -0700

    Update media.py
    Passed PEP8

--- Project 1 - Movie Trailer Website/media.py
+++ Project 1 - Movie Trailer Website/media.py
@@ -1,15 +1,18 @@
-# Importing the webbrowser allows the show_trailer function within the class Movie to open the web browser
+# Importing the webbrowser allows the show_trailer function
+# within the class Movie to open the web browser
 
 import webbrowser
 
-# This class takes 4 arguments, self, the movie's title, the poster image, and the YouTube Trailer link
+# This class takes 4 arguments:
+# self, the movie's title, the poster image, and the YouTube Trailer link
+
 
 class Movie():
-    def __init__ (self, movie_title, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
-# Opens the web browsers to the url of the movie trailer 
+
+# Opens the web browsers to the url of the movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
